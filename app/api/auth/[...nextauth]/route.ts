bfaaf127efdbcef8265d4535f1942b084b706a42
mohitj81 <Mohitjangir814@gmail.com>
2025-10-16 21:32:09 +0530

    fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,3 @@
-  export const dynamic = 'force-dynamic';
   import NextAuth from "next-auth"
   import GoogleProvider from "next-auth/providers/google"
   import { connectToDB } from "@/utils/database"
